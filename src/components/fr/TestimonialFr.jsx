5f1d29dfd55e89cadb252af83b4afb6992f8d5fb
Marco Silvera <2018019183@unfv.edu.pe>
2024-09-05 15:59:43 -0500

    Nuevo componente testimonios

---
+++ src/components/fr/TestimonialFr.jsx
@@ -0,0 +1,156 @@
+import React, { Fragment, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faAngleLeft,
+	faAngleRight,
+	faStar,
+	faStarHalfAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+
+const testimonialList = [
+	[
+		{
+			name: "Christine Gros",
+			rating: 5,
+			content:
+				'"Tout au long du séjour, nous avons apprécié la qualite et les compétences de nos guides ainsi que le professionnalisme de PACHAMAMA, petite structure souple qui sait s’adapter aux désirs de ses clients et leur proposer un programme riche et passionnant."',
+		},
+		{
+			name: "Rousseau Odile",
+			rating: 5,
+			content:
+				'"Je voulais aller voir la forêt amazonienne. Un vieux rêve que je souhaitais réaliser mais comment faire ? Je n’avais fait qu’un seul voyage, au Québec, où on parlait ma langue. Un mois avant de partir, je ne savais toujours pas comment j’allais faire pour aller voir la forêt. Une amie m’a parlé du site de Pachamama…"',
+		},
+	],
+	[
+		{
+			name: "Brigitte Van Haute",
+			rating: 5,
+			content:
+				'"Nous étions 4 personnes. Ce voyage avait été organisé par l’Agence Pachamama qui s’est montrée très ouverte à nos souhaits et nos demandes particulières. Nous avons été conseillés, guidés, transportés et accompagnés par des personnes compétentes et qui nous…"',
+		}
+	],
+];
+
+const Rating = ({ rating, showLabel, className, ...rest }) => (
+	<p className={classNames("flex flex-wrap gap-0.5", className)} {...rest}>
+		<span>
+			{[...Array(5)].map((_, i) => {
+				const index = i + 1;
+				let content = "";
+				if (index <= Math.floor(rating))
+					content = (
+						<FontAwesomeIcon
+							icon={faStar}
+							className="text-base sm:text-[22px] text-yellow-500"
+						/>
+					);
+				else if (rating > i && rating < index + 1)
+					content = (
+						<FontAwesomeIcon
+							icon={faStarHalfAlt}
+							className="text-base sm:text-[22px] text-yellow-500"
+						/>
+					);
+				else if (index > rating)
+					content = (
+						<FontAwesomeIcon
+							icon={faStar}
+							className="text-base sm:text-[22px] text-yellow-200 dark:text-opacity-20"
+						/>
+					);
+
+				return <Fragment key={i}>{content}</Fragment>;
+			})}
+		</span>
+		{showLabel && <span>{rating.toFixed(1)}</span>}
+	</p>
+);
+
+Rating.propTypes = {
+	rating: PropTypes.number.isRequired,
+	showLabel: PropTypes.bool,
+	className: PropTypes.string,
+};
+
+const TestimonialItem = ({ item }) => {
+	const { rating, content, name } = item;
+	return (
+		<div className="bg-white shadow-xl rounded-xl hover:-translate-y-1 h-full duration-300 p-5 md:min-h-[365px]">
+			<div className="mt-4">
+				<div className="flex justify-between items-center mb-6">
+					<div className="flex items-center">
+
+						<div>
+							<h5 className="text-lg sm:text-xl break-all font-medium">{name}</h5>
+						</div>
+					</div>
+					<Rating rating={rating} showLabel={false} />
+				</div>
+				<p className="leading-[1.8] opacity-80 mb-6">{content}</p>
+			</div>
+		</div>
+	);
+};
+TestimonialItem.propTypes = {
+	item: PropTypes.object.isRequired,
+};
+
+const TestimonialFr = () => {
+	const [index, setIndex] = useState(0);
+
+	const handleControl = (type) => {
+		if (type === "prev") {
+			setIndex(index <= 0 ? testimonialList.length - 1 : index - 1);
+		} else if (type === "next") {
+			setIndex(index >= testimonialList.length - 1 ? 0 : index + 1);
+		}
+	};
+
+
+	return (
+		<section className="ezy__testimonial21 light py-14 md:py-24 bg-white">
+			<div className="container px-5 mx-auto max-w-[1000px] w-full relative">
+				<div className="flex justify-center text-center mb-6 lg:mb-12">
+					<div className="max-w-lg">
+						<h2 className="text-3xl leading-none md:text-[45px] font-bold mb-6 uppercase">
+							Témoignages
+						</h2>
+						<p className="text-lg opacity-80">
+							Assumenda non repellendus distinctio nihil dicta sapiente,
+							quibusdam maiores, illum at, aliquid blanditiis eligendi qui.
+						</p>
+					</div>
+				</div>
+
+				<div className="grid grid-cols-2 gap-6 mt-12 self-center items-center justify-center">
+					{testimonialList[index].map((item, i) => (
+						<div className="col-span-2 md:col-span-1 flex justify-center items-center self-center" key={i}>
+							<TestimonialItem item={item} />
+						</div>
+					))}
+				</div>
+
+				<div className="relative flex justify-center items-center my-12">
+					<button
+						className="text-lg bg-white shadow-2xl opacity-75 hover:opacity-100 w-12 h-12 flex justify-center items-center rounded-full mr-4"
+						onClick={() => handleControl("prev")}
+					>
+						<FontAwesomeIcon icon={faAngleLeft} />
+					</button>
+					<button
+						className="text-lg bg-white shadow-2xl opacity-75 hover:opacity-100 w-12 h-12 flex justify-center items-center rounded-full"
+						onClick={() => handleControl("next")}
+					>
+						<FontAwesomeIcon icon={faAngleRight} />
+					</button>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+
+export default TestimonialFr
