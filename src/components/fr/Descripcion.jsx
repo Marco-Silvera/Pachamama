833fc003fdc384f004734680c2e8cbed45169cb4
Marco Silvera <2018019183@unfv.edu.pe>
2024-09-09 06:11:51 -0500

    Añadiendo pagina de libro de reclamaciones y su formulario

--- src/components/fr/Descripcion.jsx
+++ src/components/fr/Descripcion.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function Descripcion({ titulo, descripcion }) {
-  console.log("Descripcion props:", { titulo, descripcion });
   return (
     <section className="flex flex-col gap-5 self-center w-full mt-6 mb-20 md:mt-20 md:mb-40">
       <h2 className="text-xl md:text-3xl font-bold text-[#226428]">
