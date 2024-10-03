2866bb5e541ccc3cbe368963cef114a5561ec3f1
Marco Silvera <2018019183@unfv.edu.pe>
2024-10-02 23:37:36 -0500

    Agregando alt a los botones

--- src/components/fr/TestimonialFr.jsx
+++ src/components/fr/TestimonialFr.jsx
@@ -143,12 +143,14 @@ const TestimonialFr = () => {
 					<button
 						className="text-lg bg-white shadow-2xl opacity-75 hover:opacity-100 w-12 h-12 flex justify-center items-center rounded-full mr-4"
 						onClick={() => handleControl("prev")}
+						aria-label="Ir a los testimonios anteriores"
 					>
 						<FontAwesomeIcon icon={faAngleLeft} />
 					</button>
 					<button
 						className="text-lg bg-white shadow-2xl opacity-75 hover:opacity-100 w-12 h-12 flex justify-center items-center rounded-full"
 						onClick={() => handleControl("next")}
+						aria-label="Ir a los siguientes testimonios"
 					>
 						<FontAwesomeIcon icon={faAngleRight} />
 					</button>
